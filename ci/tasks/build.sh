bce15ea201039b1687becf30ff97c27eb00ef4c0
Thomas Detoux <thomas@detoux.fr>
2019-01-21 10:03:34 -0500

    Correcting CI

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -5,11 +5,12 @@ set -ex
 export MAVEN_USER_HOME=$(cd maven-cache && pwd)
 export MAVEN_ARGS='-Dmaven.repo.local=../maven-cache/repository'
 
-VERSION=$(<version/version)
+VERSION=$(cat version/version)
+export VERSION
 
-pushd git
+(
+    cd git
     ./mvnw install -Drevision=$VERSION $MAVEN_ARGS
 
-    cp target/* ../build-output
-    
-popd
+    cp -r target/* ../build-output
+)
