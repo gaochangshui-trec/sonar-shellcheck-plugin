1aba43f193c6b29c9459f7428058a173e63ede33
Thomas Detoux <thomas@detoux.fr>
2019-01-25 15:25:12 -0500

    Adding sonar analysis to the build

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
 set -ex
 
-export MAVEN_USER_HOME=$(cd maven-cache && pwd)
-export MAVEN_ARGS='-Dmaven.repo.local=../maven-cache/repository'
-
 VERSION=$(cat version/version)
-export VERSION
+export MAVEN_USER_HOME=$(cd maven-cache && pwd)
+MAVEN_ARGS="-Dmaven.repo.local=../maven-cache/repository -Drevision=$VERSION"
+MAVEN_ARGS="$MAVEN_ARGS -Dsonar.host.url=${SONARQUBE_URL} -Dsonar.login=${SONARQUBE_KEY}"
+
+export MAVEN_ARGS
 
 (
     cd git
-    ./mvnw install -Drevision=$VERSION $MAVEN_ARGS
+    ./mvnw org.jacoco:jacoco-maven-plugin:prepare-agent  install -Dmaven.test.failure.ignore=false $MAVEN_ARGS
+    ./mvnw sonar:sonar $MAVEN_ARGS
 
     cp -r target/* ../build-output
 )
