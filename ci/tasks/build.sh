9e895f0cae31d65e95798f0a5e87cf4ac1594e9e
Thomas Detoux <thomas@detoux.fr>
2019-03-28 19:47:40 -0400

    This is the first non-zero release of the shellcheck plugin for SonarQube.
    It uses the 5.0 version of shellcheck.
    
    Read the README for more informations.

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -4,14 +4,17 @@ set -ex
 VERSION=$(cat version/version)
 export MAVEN_USER_HOME=$(cd maven-cache && pwd)
 MAVEN_ARGS="-Dmaven.repo.local=../maven-cache/repository -Drevision=$VERSION"
-MAVEN_ARGS="$MAVEN_ARGS -Dsonar.host.url=${SONARQUBE_URL} -Dsonar.login=${SONARQUBE_KEY}"
 
 export MAVEN_ARGS
 
 (
     cd git
     ./mvnw org.jacoco:jacoco-maven-plugin:prepare-agent  install -Dmaven.test.failure.ignore=false $MAVEN_ARGS
-    ./mvnw sonar:sonar $MAVEN_ARGS
+    ./mvnw sonar:sonar $MAVEN_ARGS \
+        -Dsonar.projectKey=emerald-squad_sonar-shellcheck-plugin \
+        -Dsonar.organization=emerald-squad \
+        -Dsonar.host.url=https://sonarcloud.io \
+        -Dsonar.login=${SONARCLOUD_KEY}
 
     cp -r target/* ../build-output
 )
